42f6f7bd0a9d00cc668bcf9899e73d12c2ad85d4
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-04-05 09:53:38 -0400

    exercise 1 & 2 complete

--- challenges.js
+++ challenges.js
@@ -33,7 +33,9 @@ addOne(1) //=> 2
 addOne(-5) //=> -4
 -----------------------------------------------------------------------------*/
 // Your solution for 01-addOne here:
-
+function addOne(num) {
+  return num + 1
+}
 
 
 
@@ -57,7 +59,10 @@ addTwoNumbers(0, 0) //=> 0
 addTwoNumbers('Hello', 5) //=> NaN
 -----------------------------------------------------------------------------*/
 // Your solution for 02-addTwoNumbers here:
-
+function addTwoNumbers(num1, num2) {
+  if (isNaN(num1) || isNaN(num2)) return NaN
+  return num1 + num2
+}
 
 
 
